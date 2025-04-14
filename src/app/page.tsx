a0d16aeff67861e5f9362e3b2b06ded7d3f898d8
sbbu <jrrmyr@gmail.com>
2025-04-14 14:13:19 +0800

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAccount } from "wagmi";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { address, isConnected } = useAccount();
@@ -8,8 +9,19 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {isConnected && <p>not connected</p>}
+        <WalletConnect />
       </main>
     </div>
+  );
+}
+
+function WalletConnect() {
+  const { isConnected } = useAccount();
+
+  return (
+    <>
+      {!isConnected && <Button>Connect</Button>}
+      {isConnected && <p>connected</p>}
+    </>
   );
 }
