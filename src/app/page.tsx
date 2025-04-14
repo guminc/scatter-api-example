10f600b64a0c0425201d18ee03200558d8bde6bd
sbbu <jrrmyr@gmail.com>
2025-04-14 16:33:55 +0800

    read contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { useAppKit } from "@reown/appkit/react";
 import { useQuery } from "@tanstack/react-query";
-import { useAccount } from "wagmi";
+import { useAccount, useReadContract } from "wagmi";
 
 const SCATTER_API_URL = "https://api.scatter.art/v1";
 
@@ -14,7 +14,17 @@ const COLLECTION_SLUG = "tribe-of-girl";
 export default function Home() {
   const { address, isConnected } = useAccount();
 
-  const { data: inviteLists, isPending } = useQuery({
+  const { data: collection, isPending: isCollectionPending } = useQuery({
+    queryKey: ["collection", COLLECTION_SLUG],
+    queryFn: async () => {
+      const response = await fetch(
+        `${SCATTER_API_URL}/collection/${COLLECTION_SLUG}`
+      );
+      return response.json();
+    },
+  });
+
+  const { data: inviteLists, isPending: isInviteListsPending } = useQuery({
     queryKey: ["eligibleInviteLists", COLLECTION_SLUG, address],
     queryFn: async () => {
       const response = await fetch(
@@ -26,13 +36,16 @@ export default function Home() {
     },
   });
 
+  const isPending = isCollectionPending || isInviteListsPending;
+
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-12 font-[family-name:var(--font-geist-sans)]">
       <WalletConnect />
       <main className="flex w-xl flex-col gap-4 p-4">
-        {inviteLists?.map((list: any) => (
-          <InviteList key={list.id} list={list} />
-        ))}
+        {!isPending &&
+          inviteLists?.map((list: any) => (
+            <InviteList key={list.id} list={list} collection={collection} />
+          ))}
         {isPending && <p className="text-center">LOADING...</p>}
       </main>
     </div>
@@ -41,17 +54,26 @@ export default function Home() {
 
 function InviteList({
   list,
+  collection,
 }: {
   list: {
     id: string;
     name: string;
+    root: string;
+    address: string;
     currency_address: string;
     currency_symbol: string;
     token_price: string;
     wallet_limit: number;
     list_limit: number;
   };
+  collection: {
+    chain_id: number;
+    address: string;
+  };
 }) {
+  const { address, isConnected, chainId } = useAccount();
+
   // technically the max limit on our contract is 4294967295, we treat this as unlimited
   const MAX_LIMIT = 4294967295;
   const hasWalletLimit = list.wallet_limit !== MAX_LIMIT;
@@ -61,6 +83,25 @@ function InviteList({
     list.token_price === "0"
       ? "FREE"
       : `${list.token_price} ${list.currency_symbol}`;
+
+  const { data: listMinted } = useReadContract({
+    address: collection.address as `0x${string}`,
+    functionName: "listSupply",
+    chainId: collection.chain_id,
+    args: [list.root],
+  }) as { data: number };
+
+  const { data: walletMinted } = useReadContract({
+    address: collection.address as `0x${string}`,
+    functionName: "balanceOf",
+    chainId: collection.chain_id,
+    args: [address],
+    query: {
+      enabled: isConnected,
+    },
+  }) as { data: number };
+
+  console.log({ chainId, collection, list, listMinted, walletMinted, address });
 
   return (
     <Card>
@@ -75,14 +116,18 @@ function InviteList({
               {hasWalletLimit && (
                 <div className="h-full flex items-center flex-col gap-1">
                   <p className="text-xs opacity-50">WAL. LIMIT</p>
-                  <p>0/{list.wallet_limit}</p>
+                  <p>
+                    {walletMinted}/{list.wallet_limit}
+                  </p>
                 </div>
               )}
               {hasListLimit && (
                 <div className="h-full flex items-center flex-col gap-1">
                   <p className="text-xs opacity-50">LIMIT</p>
 
-                  <p>0/{list.list_limit}</p>
+                  <p>
+                    {listMinted}/{list.list_limit}
+                  </p>
                 </div>
               )}
             </div>
